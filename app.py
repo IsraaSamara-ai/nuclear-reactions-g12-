0525e38a45852e54604c89206fc44ec84973af23
Israa Samara <esraaysamara89@gmail.com>
2026-04-18 07:44:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import streamlit as st
 import streamlit.components.v1 as components
 
-# إعدادات الصفحة
 st.set_page_config(
     page_title="التفاعلات النووية التفاعلية",
     page_icon="⚛️",
@@ -8,11 +8,9 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# قراءة ملف HTML
 with open("nuclear_reactions.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
-# عرض التطبيق بكامل الشاشة
 components.html(
     html_content,
     height=950,
